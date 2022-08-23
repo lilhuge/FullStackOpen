793b2663e485692c4c02decee099068d70918b34
Hugo Matthews <hugo.matthews@vodafone.com>
2022-08-23 15:14:12 +0100

    2.5 course info separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,39 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((acc, part) => acc + part.exercises, 0);
-  return (
-    <p>
-      <strong>Number of exercises {sum}</strong>
-    </p>
-  );
-};
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part part={part} key={part.id} />
-      ))}
-    </>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./Course";
 
 const App = () => {
   const courses = [
