79f7d35f58713ad90cfeb329d5a3a4ec92d2c1cb
Hugo Matthews <hugo.matthews@vodafone.com>
2022-08-23 13:20:52 +0100

    2.1 course info step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,57 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+// const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} /> */}
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
