e007d35fb8515bfa93978318f913464571ddf0e7
Hugo Matthews <hugo.matthews@vodafone.com>
2022-09-20 17:30:08 +0100

    2.15 phonebook step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -75,9 +75,13 @@ const App = () => {
     if (nameAlreadyInBook) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(personObject));
-      setNewName("");
-      setNewNumber("");
+      axios
+        .post("http://localhost:3001/persons", personObject)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+          setNewName("");
+          setNewNumber("");
+        });
     }
   };
 
