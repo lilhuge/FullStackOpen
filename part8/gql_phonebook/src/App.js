f3ea5167975d1e3c19c556556d28c77412abc71e
Hugo Matthews <hugo.matthews@outlook.com>
2023-07-09 00:03:26 +0100

    Lesson 8b

---
+++ part8/gql_phonebook/src/App.js
@@ -0,0 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  return <Persons persons={result.data.allPersons} />;
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map((p) => (
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
