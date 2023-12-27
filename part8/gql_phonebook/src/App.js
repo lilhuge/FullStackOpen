58a1b3dca6a296675aa47a7201abfac336103018
Hugo Matthews <hugo.matthews@outlook.com>
2023-12-27 18:18:49 +0000

    Lesson 8b complete

--- part8/gql_phonebook/src/App.js
+++ part8/gql_phonebook/src/App.js
@@ -1,36 +1,42 @@
-import { gql, useQuery } from "@apollo/client";
+import { useState } from "react"
+import { useQuery } from "@apollo/client"
 
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
+import Persons from "./components/Persons"
+import PersonForm from "./components/PersonForm"
+import { ALL_PERSONS } from "./queries"
+import PhoneForm from "./components/PhoneForm"
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS);
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
-    return <div>loading...</div>;
+    return <div>loading...</div>
   }
 
-  return <Persons persons={result.data.allPersons} />;
-};
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
-const Persons = ({ persons }) => {
   return (
-    <div>
-      <h2>Persons</h2>
-      {persons.map((p) => (
-        <div key={p.name}>
-          {p.name} {p.phone}
-        </div>
-      ))}
-    </div>
-  );
-};
+    <>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </>
+  )
+}
 
-export default App;
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return <div style={{ color: "red" }}>{errorMessage}</div>
+}
+
+export default App
